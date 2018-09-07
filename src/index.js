2885941d34c97eb5e810937055d1d82c3e76f4e1
Kevin RRivera <krivera1015@gmail.com>
2018-09-07 10:57:48 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,65 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  //finding my elements
+  const image = document.querySelector('#image')
+  const name = document.querySelector('#name')
+  const likes = document.querySelector('#likes')
+  const commentUl = document.querySelector('#comments')
+  const likeButton = document.querySelector('#like_button')
+  const commentForm = document.querySelector('#comment_form')
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  //getting pic and its content on screen
+  PicAdapter.getPic()
+    .then(pic => {
+      console.log(pic)
+      image.src = pic.url
+      name.innerText = pic.name
+      likes.innerText = pic.like_count
+      //getting listed comments
+      pic.comments.forEach(comment => {
+        //create li element
+        const li = document.createElement('li')
+        //create deletebutton
+        li.innerText = comment.content
+        const deletebutton = document.createElement('button')
+        deletebutton.innerText = 'x'
+        li.append(deletebutton)
+        commentUl.append(li)
+      })
+    })
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  //making like button work and posting to backend
+  likeButton.addEventListener('click', (e) => {
+    //console.log(e.target.parentNode)
+    const likesId = e.target.parentNode.querySelector('#likes')
+    const likeCount = parseInt(likesId.innerText) + 1
+      PicAdapter.updateLikes({
+        image_id: 78,
+        like_count: likeCount
+      })
+        .then(pic => {
+          likesId.innerText = likeCount
+          // console.log(pic)
+        })
+  })
 
-
-
+  //get comments and updating backend
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const commentInput = document.querySelector('#comment_input')
+        PicAdapter.updateComment({
+          image_id: 78,
+          content: commentInput.value
+        })
+        .then(comment => {
+          const li = document.createElement('li')
+          li.innerText = commentInput.value
+          const deletebutton = document.createElement('button')
+          deletebutton.innerText = 'x'
+          li.append(deletebutton)
+          commentUl.append(li)
+          commentInput.value = ""
+        })
+  })
 })
-
---
+++ src/picAdapter.js
@@ -0,0 +1,38 @@
+const imageId = 78 //Enter your assigned imageId here
+
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+
+const likeURL = `https://randopic.herokuapp.com/likes/`
+
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+class PicAdapter{
+  static getPic(){
+    return fetch(imageURL)
+    .then(res => res.json())
+  }
+  static updateLikes(data){
+    return fetch(likeURL, {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+  }
+  static updateComment(data){
+    return fetch(commentsURL, {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+  }
+  static deleteComment(id, data){
+    return fetch(`${commentsURL}/${id}`, {
+      method: "PATCH",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+  }
+}
